b246c160f7f694f0c36c7524fb25c4cbb7fe3ce4
LinaDovhaniuk <lina.dovhaniuk@gmail.com>
2017-12-26 20:40:30 +0200

    deleted

--- Lab3/Lab3App/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class Lab3AppConfig(AppConfig):
-    name = 'Lab3App'
--- Lab3/SQL/lab3_event.sql
+++
@@ -1,9 +0,0 @@
-SET global event_scheduler = 1;
-
-delimiter ??
-
-create event `Lab2DB`.`delete_history`
-	on schedule every 1 day
-    do truncate table Lab2DB.History;
-??
-
